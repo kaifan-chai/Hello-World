87c3709c5ed968aaa87294634d41160c2402fcaa
kaifan <dab7765@gmail.com>
2024-06-07 01:42:40 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "head.h"
+
+int main(){
+    my_print_sring("Hello World");
+    return 0;
+}
---
+++ mycode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "head.h"
+
+void my_print_sring(char str[50]){
+    printf("%s", str);
+}
